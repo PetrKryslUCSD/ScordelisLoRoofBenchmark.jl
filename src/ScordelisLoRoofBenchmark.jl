92c4d0ca7a7ae31c10befea42e8fe15c2fbf18ef
Petr Krysl <pkrysl@ucsd.edu>
2026-08-08 11:54:32 -0700

    extrapolate minima and maxima on all edges

--- src/ScordelisLoRoofBenchmark.jl
+++ src/ScordelisLoRoofBenchmark.jl
@@ -321,7 +321,7 @@ function extrapolate_resultants(;basef = "", ns = [16, 32, 64, 128], res = "q", 
         end
         results[corner[1]] = components
     end
-    for edge in ["midsection", "diaphragm"]#["diaphragm", "peak", "free"]
+    for edge in ["midsection", "diaphragm", "peak", "free"]
         components = Dict()
         for nc in ncs
             rmx = Float64[]
@@ -345,7 +345,7 @@ function extrapolate_resultants(;basef = "", ns = [16, 32, 64, 128], res = "q", 
             else
                 verbosity > 0 && @warn "$(edge): $(res)$(nc) maximum - no result"
             end
-            components[nc] = e
+            emx = e
             r = rmn
             e = nothing
             try    
@@ -359,6 +359,8 @@ function extrapolate_resultants(;basef = "", ns = [16, 32, 64, 128], res = "q", 
             else
                 verbosity > 0 && @warn "$(edge): $(res)$(nc) minimum - no result"
             end
+            emn = e 
+            components[nc] = [emn, emx].
         end
         results[Symbol(edge)] = components
     end
